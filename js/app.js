e9a6031e838cdab516f823bf4b8a55fec512b97a
Peter Javorkai <javorkai.peter@gmail.com>
2019-02-02 15:50:43 +0100

    Added small background-color changes

--- js/app.js
+++ js/app.js
@@ -6,12 +6,13 @@ const links = document.querySelectorAll("nav a");
 const logo = document.querySelector(".about__container__logo");
 
 menuIcon.addEventListener("click", function() {
+  console.log("new");
   if (this.classList.contains("exit")) {
-    this.src = "/imgs/folk_on_the_hill_menu.svg";
+    this.src = "imgs/folk_on_the_hill_menu.svg";
     this.classList.remove("exit");
     flyIn.style.transform = "translateX(-100%)";
   } else {
-    this.src = "/imgs/folk_on_the_hill_exit.svg";
+    this.src = "imgs/folk_on_the_hill_exit.svg";
     this.classList.add("exit");
     flyIn.style.transform = "translateX(0%)";
   }
@@ -19,8 +20,7 @@ menuIcon.addEventListener("click", function() {
 
 links.forEach(link => {
   link.addEventListener("click", () => {
-    console.log(window.location);
-    menuIcon.src = `${window.location}/imgs/folk_on_the_hill_menu.svg`;
+    menuIcon.src = "imgs/folk_on_the_hill_menu.svg";
     menuIcon.classList.remove("exit");
     flyIn.style.transform = "translateX(-100%)";
   });
